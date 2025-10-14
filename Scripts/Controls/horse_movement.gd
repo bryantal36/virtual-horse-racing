0df3ea34d0273ae72e71f9509da1712475819f6e
GMallett <geoff.mallett@redmile.biz>
2025-10-14 00:40:00 -0400

    Collision Update
    Added some collision walls to the track, had the horse use CharacterBody2D and I just have it constantly bouncing whenever it collides with a wall.

--- Scripts/Controls/horse_movement.gd
+++ Scripts/Controls/horse_movement.gd
@@ -1,25 +1,38 @@
-extends Node2D
+extends CharacterBody2D
 
-@onready var horse_sprite: AnimatedSprite2D = $HorseSprite
+@onready var horse_sprite: AnimatedSprite2D = $"HorseSprite"
+
+var horse_speed = 150
 
 func _init() -> void:
-	print("test")
+	velocity = Vector2.RIGHT
 
-func _process(delta: float) -> void:
-	var horse_speed = 200
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("Up"):
-		velocity.y -= 1
-	if Input.is_action_pressed("Down"):
-		velocity.y += 1
-	if Input.is_action_pressed("Left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("Right"):
-		velocity.x += 1
-		
+func _physics_process(delta: float) -> void:
+	var collision = move_and_collide(velocity * delta * horse_speed)
+	if collision:
+		var normal = collision.get_normal()
+		velocity = velocity.bounce(normal)
+	#if Input.is_action_pressed("Up"):
+		#velocity.y -= 1
+	#if Input.is_action_pressed("Down"):
+		#velocity.y += 1
+	#if Input.is_action_pressed("Left"):
+		#velocity.x -= 1
+	#if Input.is_action_pressed("Right"):
+		#velocity.x += 1
+	
+	
+	
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * horse_speed
-		horse_sprite.play("gallop_right")
-		position += velocity * delta
+		if abs(velocity.y) <= abs(velocity.x):
+			horse_sprite.play("gallop_right")
+		if abs(velocity.y)*-1 >= velocity.x && horse_sprite.animation != "gallop_left":
+			horse_sprite.play("gallop_left")
+		if abs(velocity.y) > abs(velocity.x):
+			horse_sprite.play("gallop_down")
+		if velocity.y < abs(velocity.x)*-1:
+			horse_sprite.play("gallop_up")
 	else:
 		horse_sprite.play("idle_right")
+	
+	#linear_velocity = velocity*horse_speed*delta
